a4fd0d0aa19af3825a8bd8f9b5d49ade67317cf9
AliAskari <91182326+Alideveloperweb@users.noreply.github.com>
2023-07-08 23:49:03 +0430

    Add project files.

---
+++ Market.DataLayer/Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.DataLayer.Context
+{
+    public class ApplicationContext:DbContext
+    {
+        #region Constractor
+        public ApplicationContext(DbContextOptions<ApplicationContext>options):base(options)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Market.DataLayer/Migrations/20230708191120_CreatData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.DataLayer.Migrations
+{
+    public partial class CreatData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
